0c11d44f04787c26632126c2412fbeb4a3a2e046
Nicolas Feybesse <nfeybesse@middlewarefactory.com>
2016-07-24 13:08:47 +0200

    Fix compilation

--- src/main/java/org/genericsystem/demo/App.java
+++ src/main/java/org/genericsystem/demo/App.java
@@ -1,26 +1,18 @@
 package org.genericsystem.demo;
 
 import org.genericsystem.carcolor.CarColorApp;
-import org.genericsystem.carcolor.model.Car;
-import org.genericsystem.carcolor.model.CarColor;
-import org.genericsystem.carcolor.model.Color;
-import org.genericsystem.carcolor.model.Power;
-import org.genericsystem.common.Statics;
 import org.genericsystem.kernel.Engine;
 import org.genericsystem.reactor.appserver.ApplicationServer;
-import org.genericsystem.reactor.appserver.ApplicationsDeploymentConfig;
+import org.genericsystem.reactor.appserver.WebAppsConfig;
 import org.genericsystem.reactor.model.EngineModel;
 import org.genericsystem.todomvc.TodoApp;
 import org.genericsystem.todomvc.TodoList;
-import org.genericsystem.todomvc.Todos;
 
 public class App {
-	public static void main(String[] args) {
-		ApplicationsDeploymentConfig appsConfig = new ApplicationsDeploymentConfig(Statics.DEFAULT_HOST, args.length == 0 ? Statics.DEFAULT_PORT
-				: Integer.parseInt(args[0]));
-		appsConfig.addApplication("/todomvc", TodoApp.class, TodoList.class, Engine.class, System.getenv("HOME") + "/genericsystem/todo/", Todos.class);
-		appsConfig.addApplication("/", CarColorApp.class, EngineModel.class, Engine.class, System.getenv("HOME") + "/genericsystem/cars/", Car.class,
-				Power.class, Color.class, CarColor.class);
+	public static void main(String[] mainArgs) {
+		WebAppsConfig appsConfig = new WebAppsConfig(mainArgs);
+		appsConfig.addApplication("/todomvc", TodoApp.class, TodoList.class, Engine.class, System.getenv("HOME") + "/genericsystem/todo/");
+		appsConfig.addApplication("/", CarColorApp.class, EngineModel.class, Engine.class, System.getenv("HOME") + "/genericsystem/cars/");
 		new ApplicationServer(appsConfig).start();
 
 	}
