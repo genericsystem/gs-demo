56d7c9f0dd93bdaa4e987d532288befcafec6171
Arnaud Lievre <arnaud.lievre@gmail.com>
2016-08-25 12:30:53 +0200

    fix GS-demo

--- src/main/java/org/genericsystem/demo/App.java
+++ src/main/java/org/genericsystem/demo/App.java
@@ -4,14 +4,14 @@ import org.genericsystem.carcolor.CarColorApp;
 import org.genericsystem.kernel.Engine;
 import org.genericsystem.reactor.appserver.ApplicationServer;
 import org.genericsystem.reactor.appserver.WebAppsConfig;
+import org.genericsystem.reactor.gs.GSApp;
 import org.genericsystem.reactor.model.RootModel;
 import org.genericsystem.todomvc.TodoApp;
-import org.genericsystem.todomvc.TodoList;
 
-public class App {
+public class App extends GSApp{
 	public static void main(String[] mainArgs) {
 		WebAppsConfig appsConfig = new WebAppsConfig(mainArgs);
-		appsConfig.addApplication("/todomvc", TodoApp.class, TodoList.class, Engine.class, System.getenv("HOME") + "/genericsystem/todo/");
+		appsConfig.addApplication("/todomvc", TodoApp.class, RootModel.class, Engine.class, System.getenv("HOME") + "/genericsystem/todo/");
 		appsConfig.addApplication("/", CarColorApp.class, RootModel.class, Engine.class, System.getenv("HOME") + "/genericsystem/cars/");
 		new ApplicationServer(appsConfig).start();
 
