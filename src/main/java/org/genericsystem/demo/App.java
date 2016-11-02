1b197a2105fb0e88305e6cb78a73a4bc9cd1bfd2
Nicolas Feybesse <nfeybesse@middlewarefactory.com>
2016-11-02 08:14:02 +0100

    Fix compilation error

--- src/main/java/org/genericsystem/demo/App.java
+++ src/main/java/org/genericsystem/demo/App.java
@@ -5,14 +5,14 @@ import org.genericsystem.kernel.Engine;
 import org.genericsystem.reactor.appserver.ApplicationServer;
 import org.genericsystem.reactor.appserver.WebAppsConfig;
 import org.genericsystem.reactor.gscomponents.GSApp;
-import org.genericsystem.reactor.model.RootModel;
+import org.genericsystem.reactor.model.RootContext;
 import org.genericsystem.todomvc.TodoApp;
 
 public class App extends GSApp {
 	public static void main(String[] mainArgs) {
 		WebAppsConfig appsConfig = new WebAppsConfig(mainArgs);
-		appsConfig.addApplication("/todomvc", TodoApp.class, RootModel.class, Engine.class, System.getenv("HOME") + "/genericsystem/todo/");
-		appsConfig.addApplication("/", CarColorApp.class, RootModel.class, Engine.class, System.getenv("HOME") + "/genericsystem/cars/");
+		appsConfig.addApplication("/todomvc", TodoApp.class, RootContext.class, Engine.class, System.getenv("HOME") + "/genericsystem/todo/");
+		appsConfig.addApplication("/", CarColorApp.class, RootContext.class, Engine.class, System.getenv("HOME") + "/genericsystem/cars/");
 		new ApplicationServer(appsConfig).start();
 
 	}
