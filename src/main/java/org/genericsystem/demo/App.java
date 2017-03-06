fee566364cd2a1e58cbba8b777c65ee9b2705fa8
JClaude Kaing <jckaing@middlewarefactory.com>
2017-03-06 17:57:30 +0100

    gs-demo : added gs-quiz with "/quiz"

--- src/main/java/org/genericsystem/demo/App.java
+++ src/main/java/org/genericsystem/demo/App.java
@@ -1,11 +1,11 @@
 package org.genericsystem.demo;
 
-import org.genericsystem.reactor.gscomponents.RootTagImpl;
-
 import org.genericsystem.kernel.Engine;
+import org.genericsystem.quiz.app.QuizApp;
 import org.genericsystem.reactor.appserver.ApplicationServer;
 import org.genericsystem.reactor.appserver.WebAppsConfig;
 import org.genericsystem.reactor.context.RootContext;
+import org.genericsystem.reactor.gscomponents.RootTagImpl;
 import org.genericsystem.todomvc.TodoApp;
 
 public class App extends RootTagImpl {
@@ -13,7 +13,7 @@ public class App extends RootTagImpl {
 		WebAppsConfig appsConfig = new WebAppsConfig(mainArgs);
 		appsConfig.addApplication("/todomvc", TodoApp.class, RootContext.class, Engine.class, System.getenv("HOME") + "/genericsystem/todo/");
 		appsConfig.addApplication("/", org.genericsystem.carcolor.app.App.class, RootContext.class, Engine.class, System.getenv("HOME") + "/genericsystem/cars/");
+		appsConfig.addApplication("/quiz", QuizApp.class, RootContext.class, Engine.class, System.getenv("HOME") + "/genericsystem/quiz/");
 		new ApplicationServer(appsConfig).start();
-
 	}
 }
